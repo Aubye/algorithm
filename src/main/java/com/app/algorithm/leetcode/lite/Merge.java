b456f6072c9727d71de82d314d4b67a70361f91b
sunjinchao <sunjinchao@zhidaoauto.com>
2020-06-09 14:19:50 +0800

    modify merge algorithm

--- src/main/java/com/app/algorithm/leetcode/lite/Merge.java
+++ src/main/java/com/app/algorithm/leetcode/lite/Merge.java
@@ -6,37 +6,25 @@ import lombok.extern.slf4j.Slf4j;
 public class Merge {
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums1 = {1, 6, 7, 0, 0, 0, 0};
         int[] nums2 = {2, 5, 6};
-        merge(nums1, 3, nums2, 3);
+        merge(nums1, 0, nums2, 1);
+        log.info("nums1:{}", nums1);
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int mIndex = 0;
-        int nIndex = 0;
-        int index = 0;
-
-        int[] ints = new int[m + n];
-        while (mIndex < m && nIndex < n) {
-            int mTemp = nums1[mIndex];
-            int nTemp = nums2[nIndex];
-            if (mTemp < nTemp) {
-                ints[index++] = mTemp;
-                mIndex++;
-                continue;
-            }
-            ints[index++] = nTemp;
-            nIndex++;
+        int mIndex = m - 1;
+        int nIndex = n - 1;
+        int len = (m + n) - 1;
+        while (mIndex >= 0 && nIndex >= 0) {
+            nums1[len--] = nums1[mIndex] > nums2[nIndex] ? nums1[mIndex--] : nums2[nIndex--];
         }
-        while (mIndex < m) {
-            ints[index++] = nums1[mIndex++];
+        while (mIndex >= 0) {
+            nums1[len--] = nums1[mIndex--];
         }
-        while (nIndex < n) {
-            ints[index++] = nums2[nIndex++];
+        while (nIndex >= 0) {
+            nums1[len--] = nums2[nIndex--];
         }
-
-        nums1 = ints;
-        System.out.println(nums1.toString());
     }
 
 }
