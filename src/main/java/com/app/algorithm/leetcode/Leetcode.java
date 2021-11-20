c1ed961548c549d82f97371e1be7edc968f2d2f5
sunjinchao02 <sunjinchao02@meituan.com>
2021-11-20 23:01:10 +0800

    leetcode template

---
+++ src/main/java/com/app/algorithm/leetcode/Leetcode.java
@@ -0,0 +1,12 @@
+package com.app.algorithm.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Leetcode {
+
+    public static void main(String[] args) {
+        log.info("finish");
+    }
+
+}
